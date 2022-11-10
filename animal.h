54271bf9a4aafdb7545584d6de3b3f564c680eba
JerzyJerzu <PJ@pawlik.info.pl>
2022-11-10 20:05:20 +0100

    reproduction as method in class zoo

--- animal.h
+++ animal.h
@@ -1,16 +1,17 @@
-//#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-//using namespace std;
+using namespace std;
 class animal
 {
 	public:
 		virtual void toString() = 0;
 		virtual animal* replicate(animal* wife) = 0;
+		/*
 		string get_name();
 		int get_pExtinct();
-		int get_pBreed();
+		int get_pBreed();*/
 		string* name;
 		int* pBreed;
 		int* pExtinct;
